8c60e26b92ccdb2e0ff30812f1088825f74623b5
4itaashish <111440337+4itaashish@users.noreply.github.com>
2025-06-14 11:46:30 +0545

    Update script.js

--- script.js
+++ script.js
@@ -36,15 +36,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Season 4 team data - EDIT THESE VALUES TO UPDATE POINTS TABLE
 const season4Teams = [
-  { name: "AASHISH", mp: 1, w: 1, d: 0, l: 0, gf: 5, ga: 3, gd: 2, pts: 3 },
-  { name: "ANJAN", mp: 1, w: 0, d: 1, l: 0, gf: 2, ga: 2, gd: 0, pts: 1 },
+  { name: "AASHISH", mp: 2, w: 1, d: 0, l: 1, gf: 6, ga: 5, gd: 1, pts: 3 },
+  { name: "ANJAN", mp: 2, w: 1, d: 1, l: 0, gf: 8, ga: 4, gd: 4, pts: 4 },
   { name: "PARU DAI", mp: 0, w: 0, d: 0, l: 0, gf: 0, ga: 0, gd: 0, pts: 0 },
   { name: "RAAJ", mp: 1, w: 0, d: 1, l: 0, gf: 2, ga: 2, gd: 0, pts: 1 },
-  { name: "ANISH", mp: 0, w: 0, d: 0, l: 0, gf: 0, ga: 0, gd: 0, pts: 0 },
+  { name: "ANISH", mp: 1, w: 1, d: 0, l: 0, gf: 2, ga: 1, gd: 1, pts: 3 },
   { name: "DEV", mp: 2, w: 0, d: 1, l: 1, gf: 4, ga: 6, gd: -2, pts: 1 },
   { name: "BIKASH", mp: 1, w: 0, d: 1, l: 0, gf: 1, ga: 1, gd: 0, pts: 1 },
   { name: "ASAL", mp: 1, w: 0, d: 1, l: 0, gf: 4, ga: 4, gd: 0, pts: 1 }, // ASAL has 1 point
-  { name: "BINAYA", mp: 1, w: 0, d: 1, l: 0, gf: 4, ga: 4, gd: 0, pts: 1 },
+  { name: "BINAYA", mp: 2, w: 0, d: 1, l: 1, gf: 6, ga: 10, gd: -4, pts: 1 },
 ]
 
 // Function to sort teams according to football rules
