094101f9e7c9ed67c456d34cd18da96601a3a400
4itaashish <111440337+4itaashish@users.noreply.github.com>
2025-06-16 20:59:13 +0545

    Update script.js

--- script.js
+++ script.js
@@ -40,11 +40,11 @@ const season4Teams = [
   { name: "ANJAN", mp: 7, w: 3, d: 3, l: 1, gf: 21, ga: 11, gd: 10, pts: 12 },
   { name: "PARU DAI", mp: 4, w: 4, d: 0, l: 0, gf: 9, ga: 1, gd: 8, pts: 12 },
   { name: "RAAJ", mp: 6, w: 1, d: 2, l: 3, gf: 6, ga: 11, gd: -5, pts: 2 },
-  { name: "ANISH", mp: 5, w: 1, d: 2, l: 2, gf: 9, ga: 12, gd: -3, pts: 5 },
+  { name: "ANISH", mp: 6, w: 1, d: 2, l: 3, gf: 10, ga: 15, gd: -5, pts: 5 },
   { name: "DEV", mp: 6, w: 0, d: 1, l: 5, gf: 8, ga: 20, gd: -12, pts: 1 },
   { name: "BIKASH", mp: 3, w: 1, d: 1, l: 1, gf: 2, ga: 4, gd: -2, pts: 4 },
   { name: "ASAL", mp: 5, w: 3, d: 2, l: 0, gf: 17, ga: 10, gd: 7, pts: 11 }, // ASAL has 1 point
-  { name: "BINAYA", mp: 4, w: 1, d: 1, l: 2, gf: 10, ga: 14, gd: -4, pts: 4 },
+  { name: "BINAYA", mp: 5, w: 2, d: 1, l: 2, gf: 13, ga: 15, gd: -2, pts: 7 },
 ]
 
 // Function to sort teams according to football rules
