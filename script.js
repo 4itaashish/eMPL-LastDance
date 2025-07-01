8a0c0190cc07f2c866f47772481a52aa2f4201bf
4itaashish <111440337+4itaashish@users.noreply.github.com>
2025-07-01 21:50:28 +0545

    Update script.js

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ const season5TeamsA = [
   { name: "AASHISH", mp: 2, w: 2, d: 0, l: 0, gf: 4, ga: 1, gd: 3, pts: 6 },
   { name: "ANISH", mp: 2, w: 0, d: 0, l: 2, gf: 2, ga: 5, gd: -3, pts: 0 },
   { name: "KUSHAL", mp: 0, w: 0, d: 0, l: 0, gf: 0, ga: 0, gd: 0, pts: 0 },
-  { name: "DEV", mp: 0, w: 0, d: 0, l: 0, gf: 0, ga: 0, gd: 0, pts: 0 },
+  { name: "DEV", mp: 1, w: 0, d: 0, l: 1, gf: 0, ga: 2, gd: -2, pts: 0 },
   { name: "ASAL", mp: 1, w: 0, d: 0, l: 1, gf: 1, ga: 3, gd: -2, pts: 0 },
   { name: "RAAJ", mp: 1, w: 1, d: 0, l: 0, gf: 4, ga: 3, gd: 1, pts: 3 },
-  { name: "PARU DAI", mp: 1, w: 0, d: 0, l: 1, gf: 3, ga: 4, gd: -1, pts: 0 },
+  { name: "PARU DAI", mp: 2, w: 1, d: 0, l: 1, gf: 5, ga: 4, gd: 1, pts: 3 },
   { name: "BINAYA", mp: 1, w: 1, d: 0, l: 0, gf: 4, ga: 2, gd: 2, pts: 3 },
 ];
 
