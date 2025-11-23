d6c4f9eb97aece20c4de2fea903fc9481eea660c
4itaashish <111440337+4itaashish@users.noreply.github.com>
2025-11-23 07:26:45 +0545

    Update script.js

--- script.js
+++ script.js
@@ -20,12 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
 const season5TeamsA = [
   { name: "AASHISH", mp: 6, w: 5, d: 0, l: 1, gf: 23, ga: 14, gd: 9, pts: 15 },
   { name: "ANISH", mp: 5, w: 2, d: 2, l: 1, gf: 12, ga: 12, gd: 0, pts: 8 },
-  { name: "SADIP", mp: 2, w: 0, d: 0, l: 2, gf: 0, ga: 3, gd: -3, pts: 0 },
+  { name: "SADIP", mp: 4, w: 1, d: 0, l: 3, gf: 3, ga: 3, gd: 0, pts: 3 },
   { name: "BINAYA", mp: 5, w: 3, d: 0, l: 2, gf: 16, ga: 11, gd: 5, pts: 9 },
   { name: "RAAJ", mp: 6, w: 0, d: 0, l: 6, gf: 3, ga: 14, gd: -11, pts: 0 },
   { name: "PARU DAI", mp: 5, w: 4, d: 0, l: 1, gf: 16, ga: 7, gd: 9, pts: 12 },
-  { name: "ASAL", mp: 6, w: 0, d: 1, l: 5, gf: 7, ga: 19, gd: -11, pts: 1 },
-  { name: "PRADIP", mp: 4, w: 1, d: 0, l: 3, gf: 6, ga: 8, gd: -2, pts: 3 },
+  { name: "ASAL", mp: 7, w: 0, d: 1, l: 6, gf: 7, ga: 22, gd: -14, pts: 1 },
+  { name: "PRADIP", mp: 5, w: 1, d: 0, l: 4, gf: 6, ga: 8, gd: -2, pts: 3 },
   { name: "ANJAN", mp: 6, w: 4, d: 1, l: 1, gf: 17, ga: 11, gd: 6, pts: 13 },
   { name: "DEV", mp: 5, w: 3, d: 0, l: 2, gf: 14, ga: 8, gd: 6, pts: 9 },
   { name: "BIKASH", mp: 4, w: 1, d: 0, l: 3, gf: 6, ga: 13, gd: -7, pts: 3 },
@@ -350,6 +350,7 @@ function highlightAashishMatches() {
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(highlightAashishMatches, 500)
 })
+
 
 
 
