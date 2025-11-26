03f1b5c45910c337e5402b48d7bc1b7ffae206f3
4itaashish <111440337+4itaashish@users.noreply.github.com>
2025-11-26 20:28:10 +0545

    Update script.js

--- script.js
+++ script.js
@@ -21,9 +21,7 @@ const season5TeamsA = [
   { name: "AASHISH", mp: 10, w: 7, d: 1, l: 2, gf: 31, ga: 19, gd: 12, pts: 22 },
   { name: "ANISH", mp: 11, w: 7, d: 2, l: 2, gf: 35, ga: 19, gd: 16, pts: 23 },
   { name: "BINAYA", mp: 10, w: 8, d: 0, l: 2, gf: 39, ga: 21, gd: 18, pts: 24 },
-  { name: "RAAJ", mp: 11, w: 0, d: 0, l: 11, gf: 3, ga: 29, gd: -26, pts: 0 },
   { name: "PARU DAI", mp: 11, w: 8, d: 1, l: 2, gf: 30, ga: 14, gd: 16, pts: 25 },
-  { name: "ASAL", mp: 12, w: 2, d: 1, l: 9, gf: 16, ga: 36, gd: -20, pts: 7 },
   { name: "PRADIP", mp: 11, w: 1, d: 1, l: 9, gf: 22, ga: 34, gd: -12, pts: 4 },
   { name: "ANJAN", mp: 11, w: 8, d: 1, l: 2, gf: 31, ga: 20, gd: 11, pts: 25 },
   { name: "DEV", mp: 11, w: 6, d: 1, l: 4, gf: 27, ga: 16, gd: 11, pts: 19 },
@@ -349,6 +347,7 @@ function highlightAashishMatches() {
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(highlightAashishMatches, 500)
 })
+
 
 
 
