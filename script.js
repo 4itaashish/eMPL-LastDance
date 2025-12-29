fa8a4752368b6556a1fada77df6b49c2f6d0ab09
4itaashish <111440337+4itaashish@users.noreply.github.com>
2025-12-29 20:56:33 +0545

    Update script.js

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ const season5TeamsA = [
   { name: "AASHISH", mp: 3, w: 2, d: 1, l: 0, gf: 9, ga: 4, gd: 5, pts: 7 },
   { name: "ANISH", mp: 3, w: 2, d: 0, l: 1, gf: 7, ga: 4, gd: 3, pts: 6 },,
   { name: "SUNIL", mp: 3, w: 0, d: 0, l: 3, gf: 2, ga: 10, gd: -8, pts: 0 },
-  { name: "ANJAN", mp: 3, w: 2, d: 1, l: 0, gf: 8, ga: 5, gd: 3, pts: 7 },
+  { name: "ANJAN", mp: 4, w: 2, d: 1, l: 1, gf: 8, ga: 7, gd: 1, pts: 7 },
   { name: "DEV", mp: 2, w: 0, d: 0, l: 2, gf: 2, ga: 9, gd: -7, pts: 0 },
   { name: "BIKASH", mp: 1, w: 1, d: 0, l: 0, gf: 4, ga: 3, gd: 1, pts: 3 },
   { name: "RAAJ", mp: 3, w: 1, d: 0, l: 2, gf: 7, ga: 9, gd: -2, pts: 3 },
-  { name: "BINAYA", mp: 3, w: 1, d: 0, l: 2, gf: 9, ga: 7, gd: 2, pts: 3 },
+  { name: "BINAYA", mp: 4, w: 2, d: 0, l: 2, gf: 11, ga: 7, gd: 4, pts: 6 },
   { name: "PARU DAI", mp: 3, w: 2, d: 0, l: 1, gf: 9, ga: 6, gd: 3, pts: 6 },
 ];
 
@@ -348,6 +348,7 @@ function highlightAashishMatches() {
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(highlightAashishMatches, 500)
 })
+
 
 
 
