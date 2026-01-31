cb251a1a38d0384cd53e697d534d0f98935a69c2
4itaashish <111440337+4itaashish@users.noreply.github.com>
2026-01-31 21:02:43 +0545

    Update script.js

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Season 5 team data - Group A
 const season5TeamsA = [
-  { name: "AASHISH", mp: 5, w: 4, d: 1, l: 0, gf: 18, ga: 8, gd: 10, pts: 13 },
+  { name: "AASHISH", mp: 6, w: 5, d: 1, l: 0, gf: 21, ga: 8, gd: 13, pts: 16 },
   { name: "SUNIL", mp: 4, w: 2, d: 0, l: 2, gf: 12, ga: 13, gd: -1, pts: 6 },
   { name: "BIKASH", mp: 5, w: 2, d: 1, l: 2, gf: 10, ga: 11, gd: -1, pts: 7 },
-  { name: "RAAJ", mp: 3, w: 0, d: 1, l: 2, gf: 7, ga: 11, gd: -4, pts: 1 },
-  { name: "ANJAN", mp: 3, w: 0, d: 0, l: 3, gf: 3, ga: 8, gd: -5, pts: 0 },
-  { name: "DEV", mp: 4, w: 2, d: 1, l: 1, gf: 10, ga: 9, gd: 1, pts: 7 },
+  { name: "RAAJ", mp: 6, w: 0, d: 1, l: 5, gf: 7, ga: 20, gd: -13, pts: 1 },
+  { name: "ANJAN", mp: 4, w: 1, d: 0, l: 3, gf: 6, ga: 8, gd: -2, pts: 3 },
+  { name: "DEV", mp: 5, w: 3, d: 1, l: 1, gf: 13, ga: 9, gd: 4, pts: 10 },
 ];
 
 // Season 5 team data - Group B
