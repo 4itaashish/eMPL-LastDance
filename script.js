73c5735eaab87bbdc889550d3c7d96ec2a28f63d
4itaashish <111440337+4itaashish@users.noreply.github.com>
2026-03-19 23:39:48 +0545

    Update script.js

--- script.js
+++ script.js
@@ -18,17 +18,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Season 5 team data - Group A
 const season5TeamsA = [
-  { name: "AASHISH", mp: 2, w: 2, d: 0, l: 0, gf: 8, ga: 4, gd: 4, pts: 6 },
-  { name: "SUNIL", mp: 5, w: 2, d: 1, l: 2, gf: 18, ga: 19, gd: -1, pts: 7 },
-  { name: "BIKASH", mp: 2, w: 0, d: 0, l: 2, gf: 5, ga: 9, gd: -4, pts: 0 },
-  { name: "PARU DAI", mp: 3, w: 1, d: 0, l: 2, gf: 6, ga: 7, gd: -1, pts: 3 },
-  { name: "ANJAN", mp: 4, w: 3, d: 0, l: 1, gf: 16, ga: 9, gd: 7, pts: 9 },
-  { name: "DEV", mp: 2, w: 0, d: 0, l: 2, gf: 4, ga: 7, gd: -3, pts: 0 },
-  { name: "BINAYA", mp: 2, w: 0, d: 0, l: 2, gf: 5, ga: 11, gd: -6, pts: 0 },
-  { name: "ASAL", mp: 4, w: 1, d: 0, l: 3, gf: 11, ga: 21, gd: -10, pts: 3 },
-  { name: "ANISH", mp: 4, w: 3, d: 1, l: 0, gf: 16, ga: 8, gd: 8, pts: 10 },
-  { name: "SADIP", mp: 1, w: 1, d: 0, l: 0, gf: 7, ga: 4, gd: 3, pts: 3 },
-  { name: "RAAJ", mp: 3, w: 2, d: 0, l: 1, gf: 11, ga: 8, gd: 3, pts: 6 },
+  { name: "AASHISH", mp: 4, w: 4, d: 0, l: 0, gf: 17, ga: 4, gd: 13, pts: 12 },
+  { name: "SUNIL", mp: 6, w: 3, d: 1, l: 2, gf: 22, ga: 19, gd: 3, pts: 10 },
+  { name: "BIKASH", mp: 3, w: 0, d: 0, l: 3, gf: 5, ga: 14, gd: -9, pts: 0 },
+  { name: "PARU DAI", mp: 4, w: 1, d: 0, l: 3, gf: 8, ga: 10, gd: -2, pts: 3 },
+  { name: "ANJAN", mp: 6, w: 4, d: 0, l: 2, gf: 22, ga: 15, gd: 7, pts: 12 },
+  { name: "DEV", mp: 5, w: 1, d: 0, l: 4, gf: 8, ga: 16, gd: -8, pts: 3 },
+  { name: "BINAYA", mp: 4, w: 1, d: 0, l: 3, gf: 11, ga: 15, gd: -4, pts: 3 },
+  { name: "ASAL", mp: 5, w: 1, d: 0, l: 4, gf: 13, ga: 24, gd: -11, pts: 3 },
+  { name: "ANISH", mp: 5, w: 3, d: 1, l: 1, gf: 17, ga: 13, gd: 4, pts: 10 },
+  { name: "SADIP", mp: 4, w: 4, d: 0, l: 0, gf: 19, ga: 10, gd: 9, pts: 12 },
+  { name: "RAAJ", mp: 6, w: 3, d: 0, l: 3, gf: 15, ga: 17, gd: -2, pts: 9 },
 ];
 
 // Season 5 team data - Group B
