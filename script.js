46903e048edddf9f9da0884a8f04b1cbd15ae33a
4itaashish <111440337+4itaashish@users.noreply.github.com>
2026-03-25 22:16:39 +0545

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Season 5 team data - Group A
 const season5TeamsA = [
-  { name: "AASHISH", mp: 12, w: 11, d: 0, l: 1, gf: 51, ga: 21, gd: 30, pts: 33 },
-  { name: "SUNIL", mp: 11, w: 3, d: 3, l: 5, gf: 33, ga: 38, gd: -5, pts: 12 },
+  { name: "AASHISH", mp: 13, w: 12, d: 0, l: 1, gf: 55, ga: 24, gd: 31, pts: 36 },
+  { name: "SUNIL", mp: 12, w: 3, d: 3, l: 6, gf: 36, ga: 42, gd: -6, pts: 12 },
   { name: "BIKASH", mp: 11, w: 3, d: 1, l: 7, gf: 39, ga: 59, gd: -20, pts: 10 },
   { name: "PARU DAI", mp: 14, w: 5, d: 3, l: 6, gf: 41, ga: 39, gd: 2, pts: 18 },
   { name: "ANJAN", mp: 11, w: 5, d: 1, l: 5, gf: 36, ga: 32, gd: 4, pts: 16 },
