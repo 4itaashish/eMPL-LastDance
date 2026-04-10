8588cd83f0ddc9295058851a33529c0735b0b77b
4itaashish <111440337+4itaashish@users.noreply.github.com>
2026-04-10 21:44:48 +0545

    Update script.js

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Season 5 team data - Group A
 const season5TeamsA = [
-  { name: "AASHISH", mp: 17, w: 13, d: 1, l: 3, gf: 75, ga: 41, gd: 34, pts: 40 },
+  { name: "AASHISH", mp: 18, w: 13, d: 1, l: 4, gf: 76, ga: 43, gd: 33, pts: 40 },
   { name: "SUNIL", mp: 19, w: 4, d: 6, l: 9, gf: 52, ga: 62, gd: -10, pts: 18 },
   { name: "BIKASH", mp: 15, w: 4, d: 2, l: 9, gf: 54, ga: 75, gd: -21, pts: 14 },
   { name: "PARU DAI", mp: 19, w: 5, d: 5, l: 9, gf: 53, ga: 55, gd: -2, pts: 20 },
   { name: "ANJAN", mp: 18, w: 10, d: 3, l: 5, gf: 64, ga: 49, gd: 15, pts: 33 },
-  { name: "DEV", mp: 15, w: 5, d: 2, l: 8, gf: 35, ga: 46, gd: -11, pts: 17 },
+  { name: "DEV", mp: 16, w: 6, d: 2, l: 8, gf: 37, ga: 47, gd: -10, pts: 20 },
   { name: "BINAYA", mp: 18, w: 9, d: 4, l: 5, gf: 62, ga: 53, gd: 9, pts: 31 },
   { name: "ASAL", mp: 19, w: 5, d: 1, l: 13, gf: 42, ga: 69, gd: -27, pts: 16 },
   { name: "ANISH", mp: 17, w: 9, d: 2, l: 6, gf: 50, ga: 51, gd: -1, pts: 29 },
